11f28632f486590127c167bba952546cecf185cf
MD <472320501@qq.com>
2017-05-08 17:18:28 +0800

    这是微信小程序的第二次提交，已经完成

--- pages/bookdetail/bookdetail.js
+++ pages/bookdetail/bookdetail.js
@@ -1,19 +1,25 @@
 // pages/bookdetail/bookdetail.js
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+    singlebook: {}
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var that=this;
+    console.log(options.id);
+    wx.request({
+      url: 'https://api.douban.com/v2/book/' + options.id,
+      method: 'GET',
+      success: function (res) {
+        // success
+        console.log(res);
+        that.setData({
+          singlebook:res.data
+        });
+      }
+    })
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
   }
 })
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,28 +1,43 @@
-function getRandomColor () {
-  let rgb = []
-  for (let i = 0 ; i < 3; ++i){
-    let color = Math.floor(Math.random() * 256).toString(16)
-    color = color.length == 1 ? '0' + color : color
-    rgb.push(color)
-  }
-  return '#' + rgb.join('')
-}
-
+// pages/mine/mine.js
 Page({
-  onReady: function (res) {
-    this.videoContext = wx.createVideoContext('myVideo')
+  data: {
+    info: {}
   },
-  inputValue: '',
-    data: {
-        
-    },
-  bindInputBlur: function(e) {
-    this.inputValue = e.detail.value
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    var app = getApp();
+    var that = this;
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      console.log(userInfo);
+      that.setData({
+        info: userInfo
+      })
+    })
+
+    console.log(this.data.info);
+
   },
-  bindSendDanmu: function () {
-    this.videoContext.sendDanmu({
-      text: this.inputValue,
-      color: getRandomColor()
+  saoyisao: function () {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  },
+  location: function () {
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      }
     })
   }
 })
+
